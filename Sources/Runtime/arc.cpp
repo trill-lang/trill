a50c9e5069a7dd79a362cbdd3e1c47d8afbbc845
Harlan Haskins <harlan@harlanhaskins.com>
2017-03-07 19:39:10 -0500

    Added better error detection and failure reporting to RefCountBox.

--- Sources/Runtime/arc.cpp
+++ Sources/Runtime/arc.cpp
@@ -12,6 +12,7 @@
 #include <atomic>
 #include <mutex>
 #include <iostream>
+#include <sstream>
 #include <chrono>
 
 #define DEBUG_ARC
@@ -20,8 +21,8 @@
 #define DEBUG_ARC_LOG(x) do { \
    auto time = std::chrono::system_clock::now(); \
    auto timeVal = std::chrono::system_clock::to_time_t(time); \
-   std::cout << std::ctime(&timeVal) << "  " x << " (" << box->value() \
-             << ") -- retain count is now " << box->retainCount << std::endl; \
+   std::cout << std::ctime(&timeVal) << "  " << x << " (" << value \
+             << ") -- retain count is now " << box()->retainCount << std::endl; \
 } while (false)
 #else
 #define DEBUG_ARC_LOG(x) ({})
@@ -33,30 +34,23 @@ namespace trill {
  A RefCountBox contains
   - A retain count
   - A pointer to the type's deinitializer
+  - A mutex used to synchronize retains/releases
   - A variably-sized payload that is not represented by a data member.
 
   It is meant as a hidden store for retain count data alongside the allocated
   contents of an indirect type.
 
   Trill will always see this as:
-      [retainCount][deinitializer][payload]
-                                  ^~ indirect type "begins" here
+      [box reference (RefCountBox *)][payload (void *)]
+                                     ^~ indirect type "begins" here
  */
 struct RefCountBox {
   uint32_t retainCount;
   trill_deinitializer_t deinit;
-  std::mutex *mutex;
+  std::mutex mutex;
 
   RefCountBox(uint32_t retainCount, trill_deinitializer_t deinit):
-    retainCount(retainCount), deinit(deinit) {
-      this->mutex = new std::mutex();
-    }
-
-  /// Finds the payload by walking to the end of the data members of this box.
-  void *value() {
-    return reinterpret_cast<void *>(reinterpret_cast<uintptr_t>(this) +
-                                    sizeof(RefCountBox));
-  }
+    retainCount(retainCount), deinit(deinit) {}
 };
 
 /**
@@ -65,61 +59,80 @@ struct RefCountBox {
  */
 struct RefCounted {
 public:
-  RefCountBox *box;
+  RefCountBox **boxPtr;
+  void *value;
 
   /**
-   Creates a \c RefCountBox along with a payload of the specific size.
+   Creates a \c RefCounted along with a payload of the specific size.
    @param size The size of the underlying payload.
    @param deinit The deinitializer for the type being created.
    */
-  static RefCountBox *createBox(size_t size, trill_deinitializer_t deinit) {
-    auto boxPtr = trill_alloc(sizeof(RefCountBox) + size);
-    auto box = reinterpret_cast<RefCountBox *>(boxPtr);
-    *box = RefCountBox(0, deinit);
+  RefCounted(size_t size, trill_deinitializer_t deinit) {
+    auto boxFullPtr = trill_alloc(sizeof(RefCountBox *) + size);
+    boxPtr = reinterpret_cast<RefCountBox **>(boxFullPtr);
+    *boxPtr = new RefCountBox(0, deinit);
+    value = reinterpret_cast<void *>(
+              reinterpret_cast<uintptr_t>(boxFullPtr) + sizeof(boxPtr));
     DEBUG_ARC_LOG("creating box");
-    return box;
   }
 
   /**
    Gets a \c RefCounted instance for a given pointer into a box, by offsetting
    the value pointer with the size of the \c RefCountBox.
    
-   @param boxValue The payload value underlying a \c RefCountBox.
+   @param boxedValue The payload value underlying a \c RefCountBox.
    */
-  RefCounted(void *_Nonnull boxValue) {
-    auto boxPtr = reinterpret_cast<uintptr_t>(boxValue) - sizeof(RefCountBox);
-    box = reinterpret_cast<RefCountBox *>(boxPtr);
+  RefCounted(void *_Nonnull boxedValue) {
+    value = boxedValue;
+    boxPtr = reinterpret_cast<RefCountBox **>(
+               reinterpret_cast<uintptr_t>(value) - sizeof(RefCountBox **));;
+  }
+
+  /**
+   Reaches into the payload to find the reference counted box.
+   */
+  RefCountBox *box() {
+    return *boxPtr;
+  }
+
+  void checkForUseAfterDealloc() {
+    if (box() == nullptr) {
+      std::stringstream msg;
+      msg << "object (" << value << ") used after deallocation";
+      trill_fatalError(msg.str().c_str());
+    }
   }
 
   /**
    Determines if this object's reference count is exactly one.
    */
   bool isUniquelyReferenced() {
-    trill_assert(box != nullptr);
-    std::lock_guard<std::mutex> guard(*box->mutex);
-    return box->retainCount == 1;
+    checkForUseAfterDealloc();
+    std::lock_guard<std::mutex> guard(box()->mutex);
+    return box()->retainCount == 1;
   }
 
   /**
    Gets the current retain count of an object.
    */
   uint32_t retainCount() {
-    trill_assert(box != nullptr);
-    std::lock_guard<std::mutex> guard(*box->mutex);
+    checkForUseAfterDealloc();
+    std::lock_guard<std::mutex> guard(box()->mutex);
     DEBUG_ARC_LOG("getting retain count");
-    return box->retainCount;
+    return box()->retainCount;
   }
 
   /**
    Retains the value inside a \c RefCountBox.
    */
   void retain() {
-    trill_assert(box != nullptr);
-    std::lock_guard<std::mutex> guard(*box->mutex);
-    if (box->retainCount == std::numeric_limits<decltype(box->retainCount)>::max()) {
+    checkForUseAfterDealloc();
+    DEBUG_ARC_LOG(value);
+    std::lock_guard<std::mutex> guard(box()->mutex);
+    if (box()->retainCount == std::numeric_limits<decltype(box()->retainCount)>::max()) {
       trill_fatalError("retain count overflow");
     }
-    box->retainCount++;
+    box()->retainCount++;
     DEBUG_ARC_LOG("retaining object");
   }
 
@@ -128,20 +141,20 @@ public:
    this method is called, then the object will be explicitly deallocated.
    */
   void release() {
-    trill_assert(box != nullptr);
-    box->mutex->lock();
-    if (box->retainCount == 0) {
+    checkForUseAfterDealloc();
+    box()->mutex.lock();
+    if (box()->retainCount == 0) {
       trill_fatalError("attempting to release object with retain count 0");
     }
 
-    box->retainCount--;
+    box()->retainCount--;
 
     DEBUG_ARC_LOG("releasing object");
 
-    if (box->retainCount == 0) {
+    if (box()->retainCount == 0) {
       dealloc(); // mutex will be unlocked and invalidated
     } else {
-      box->mutex->unlock(); // otherwise manually unlock
+      box()->mutex.unlock(); // otherwise manually unlock
     }
   }
 
@@ -151,33 +164,30 @@ private:
    @note This function *must* be called with a locked \c mutex.
    */
   void dealloc() {
-    trill_assert(box != nullptr);
+    checkForUseAfterDealloc();
 
-    if (box->retainCount > 0) {
+    if (box()->retainCount > 0) {
       trill_fatalError("object deallocated with retain count > 0");
     }
 
     DEBUG_ARC_LOG("deallocating");
 
-    if (box->deinit != nullptr) {
-      box->deinit(box->value());
+    if (box()->deinit != nullptr) {
+      box()->deinit(value);
     }
 
     // Explicitly unlock the mutex before deleting it
-    box->mutex->unlock();
+    box()->mutex.unlock();
+    delete box();
 
-    delete box->mutex;
-
-    // Cannot delete the box because it was allocated manually.
-    free(box);
-
-    box = nullptr;
+    // Erase the box's location
+    *boxPtr = nullptr;
   }
 };
 
 void *_Nonnull trill_allocateIndirectType(size_t size,
                                           trill_deinitializer_t deinit) {
-  return RefCounted::createBox(size, deinit)->value();
+  return RefCounted(size, deinit).value;
 }
 
 void trill_retain(void *_Nonnull instance) {
